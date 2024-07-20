11665ec8335971b8aacec0ef2a526f463eb5c7b3
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-07-20 23:38:44 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,7 +14,6 @@ export default function App() {
   const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [perPage, setPerPage] = useState(INITIAL_PER_PAGE);
   const [showModal, setShowModal] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
 
@@ -25,17 +24,16 @@ export default function App() {
   }, [query]);
 
   const handleSearchSubmit = async searchQuery => {
-    setQuery(searchQuery);
     setLoading(true);
     try {
       const response = await axios.get(
-        `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+        `https://pixabay.com/api/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&per_page=${INITIAL_PER_PAGE}`
       );
       setImages(response.data.hits);
-      setLoading(false);
     } catch (error) {
       console.error('Error searching images:', error);
       setError('Error searching images');
+    } finally {
       setLoading(false);
     }
   };
@@ -44,15 +42,15 @@ export default function App() {
     event.preventDefault();
     setLoading(true);
     try {
-      const nextPage = Math.ceil(images.length / perPage) + 1;
+      const nextPage = Math.ceil(images.length / INITIAL_PER_PAGE) + 1;
       const response = await axios.get(
-        `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&per_page=${perPage}&page=${nextPage}`
+        `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&per_page=${INITIAL_PER_PAGE}&page=${nextPage}`
       );
       setImages(prevImages => [...prevImages, ...response.data.hits]);
-      setLoading(false);
     } catch (error) {
       console.error('Error loading more images:', error);
       setError('Error loading more images');
+    } finally {
       setLoading(false);
     }
   };
